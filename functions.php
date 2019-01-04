76e3246c1e6ac4a52acd30516fed6859b5f10d8f
Matthew Barry <mbarry5@mail.greenriver.edu>
2019-01-04 13:59:21 -0500

    added average of array function

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 
 	function printer($array)
 	{
-		for ($i = 0; $i< sizeof($array) ; $i++)
+		for ($i = 0; $i  < sizeof($array) ; $i++)
 		{
 			echo $array[$i] .", ";
 		}
@@ -17,4 +17,14 @@
 	function largest($array)
 	{
 		return max($array);
+	}
+
+	function average($array)
+	{
+		//divide array sum by the array length to give you the average
+		$arraySum = array_sum($array);//Sum of array
+		$arrayLength = count($array);//length of array
+
+		return ($arraySum/$arrayLength);
+
 	}
