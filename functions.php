1d2b04c9b57f6fb10cea24af790a10e20dc27da3
Matthew Barry <mbarry5@mail.greenriver.edu>
2019-01-04 16:58:15 -0500

    Added a remove duplicates function

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 
 	function printer($array)
 	{
-		for ($i = 0; $i  < sizeof($array) ; $i++)
+		for ($i = 0; $i  < count($array) ; $i++)
 		{
 			echo $array[$i] .", ";
 		}
@@ -26,5 +26,17 @@
 		$arrayLength = count($array);//length of array
 
 		return ($arraySum/$arrayLength);
+	}
 
+	function removeDups($array)
+	{
+		//remove duplicates from the array
+		$arraySlim = array_unique($array);
+		for ($i = 0; $i  < count($array) ; $i++)
+		{
+			if(!$arraySlim[$i] == NULL)
+			{
+				echo $arraySlim[$i] .", ";
+			}
+		}
 	}
